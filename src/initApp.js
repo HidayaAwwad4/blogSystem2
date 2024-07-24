a03f1e9f262259a770f846a933d8ab016e4dceee
hidaya <hidayaawad4@gmail.com>
2024-07-24 17:14:08 +0300

    update databas

--- src/initApp.js
+++ src/initApp.js
@@ -3,10 +3,10 @@ import blogRouter from './modules/blog/blog.router.js';
 import authRouter from './modules/auth/auth.router.js';
 import commentRouter from './modules/comment/comment.router.js';
 import dbconnection from '../DB/connection.js';
-//import cors from 'cors';
+import cors from 'cors';
 export const initApp = (app,express)=>{
     dbconnection();
-    //app.use(cors());
+    app.use(cors());
     app.use(express.json());
     app.use('/auth',authRouter);
     app.use('/users',userRouter);
